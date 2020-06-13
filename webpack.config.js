c88723fd6344244599da1f02f7af7bd081eb6688
yerbol kosbayev <yerbolkosbayev@gmail.com>
2020-06-13 12:14:09 +0600

    created webpack config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+    ],
+  },
+};
