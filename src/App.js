a52954858663a627bc7da0281dcd4721ef9dee6c
yerbol kosbayev <yerbolkosbayev@gmail.com>
2020-06-16 20:44:49 +0600

    next

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import { getAscii } from "./helpers/getProducts";
 import { ThisContext } from "./index";
@@ -10,22 +10,52 @@ export default () => {
   const { state, dispatch } = useContext(ThisContext);
   const [isFetching, setIsFetching] = useState(false);
 
+  // useEffect(() => {
+  //   getAscii(1, 20, dispatch);
+  // }, []);
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", onScroll);
+  // }, [state, onScroll]);
+
+  // const onScroll = () => {
+  //   if (
+  //     window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+  //     state.more == false &&
+  //     state.loading == false
+  //   ) {
+  //     getAscii(state.page, 21, dispatch);
+  //   }
+  // };
+
   useEffect(() => {
-    getAscii(1, 20, dispatch);
+    getAscii(1, 20, dispatch, state.sortBy, false);
+    console.log("first use effect is run");
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  useEffect(() => {
-    window.addEventListener("scroll", onScroll);
-  }, [state, onScroll]);
-
-  const onScroll = () => {
+  function handleScroll() {
     if (
-      window.innerHeight + window.scrollY >= document.body.offsetHeight &&
-      state.more == false &&
-      state.loading == false
-    ) {
-      getAscii(state.page, 21, dispatch);
-    }
+      window.innerHeight + document.documentElement.scrollTop !==
+        document.documentElement.offsetHeight ||
+      isFetching
+    )
+      return;
+    setIsFetching(true);
+  }
+
+  useEffect(() => {
+    if (!isFetching) return;
+    fetchMoreListItems();
+  }, [isFetching, state.data]);
+
+  const fetchMoreListItems = () => {
+    // console.log('fetchMoreListItems runs')
+    getAscii(state.page, 20, dispatch, state.sortBy, false);
+    setTimeout(() => {
+      setIsFetching(false);
+    }, 1500);
   };
 
   const addRender = (id) => {
@@ -34,37 +64,35 @@ export default () => {
         <td>
           <img src={`/ads/?r=6`} />
         </td>
-        {/* {hasMore()} */}
+        {HasMore()}
+        {End()}
       </tr>
     );
   };
 
   const HasMore = () =>
-    (state.more = true ? (
-      <tr>
-        <td>loading</td>
-      </tr>
-    ) : (
-      <tr>
-        <td>NOT</td>
-      </tr>
-    ));
+    isFetching && (
+      <td>
+        <Spinner />
+      </td>
+    );
+  const End = () => state.more == false && <td>~ end of catalogue ~</td>;
 
-  const table = () => {
+  const Table = () => {
     return (
-      <table className='container'>
+      <Table className='container'>
         <thead>
           <tr>
-            <th onClick={() => console.log("ID clicked")}>
+            <th onClick={() => getAscii(1, 20, dispatch, "id", true)}>
               <h1>ID</h1>
             </th>
-            <th>
+            <th onClick={() => getAscii(1, 20, dispatch, "size", true)}>
               <h1>Size</h1>
             </th>
             <th>
               <h1>Face</h1>
             </th>
-            <th>
+            <th onClick={() => getAscii(1, 20, dispatch, "price", true)}>
               <h1>Price</h1>
             </th>
             <th>
@@ -78,9 +106,7 @@ export default () => {
           let currencyString = "$" + (r.price / 100).toFixed(2);
           return (
             <tbody key={r.id}>
-              {i !== 0 && i % 20 === 0 && addRender(r.id)}
-              {i !== 0 && i % 20 === 0 && hasMore()}
-              {/* {i !== 0 && i % 20 === 0 &&  <HasMore />} */}
+              {i !== 0 && (i + 1) % 20 === 0 && addRender(r.id)}
 
               <tr>
                 <td>{r.id}</td>
@@ -92,7 +118,7 @@ export default () => {
             </tbody>
           );
         })}
-      </table>
+      </Table>
     );
   };
 
--- src/helpers/getProducts.js
+++ src/helpers/getProducts.js
@@ -1,30 +1,41 @@
 import { Config } from "./config";
 
 let cachePage = null;
-export const getAscii = async (page, limit = "10", dispatch) => {
-  if (cachePage === page || cachePage > page) {
+let cachesortBy = null;
+export const getAscii = async (page, limit = "20", dispatch, sortBy, reset) => {
+  if (cachePage === page && cachePage > page) {
+    console.log("condition not met");
     return null;
   } else {
-    console.log("get data is running with page ", page);
-  }
-
-  try {
-    const url = `${Config.API_URL}products?_page=${page + 1}&_limit=${limit}`;
-    fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((pre) => pre.json())
-      .then((pre) => {
-        dispatch({ type: "preLoadItems", pre });
+    // console.log('get data is running with page ', page)
+    reset && dispatch({ type: "resetandsort", model: sortBy });
+    page == 1 && dispatch({ type: "loadingTrue" });
+    try {
+      const url = `${Config.API_URL}products?_page=${page}&_limit=${limit}&_sort=${sortBy}`;
+      fetch(url, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+        },
       })
-      .then(() => dispatch({ type: "loadingFalse" }));
-  } catch (error) {
-    console.log(error);
+        .then((pre) => pre.json())
+        .then((pre) => {
+          pre.length < 1
+            ? dispatch({ type: "nomore" })
+            : dispatch({ type: "preLoadItems", pre });
+          // pre.length < 1 ? dispatch({ type: 'nomore' }) : dispatch({ type: 'preLoadItems', pre })
+          // pre.length < 1 ? dispatch({ type: 'nomore' }) : dispatch({ type: 'preLoadItems', pre })
+          // dispatch({ type: 'preLoadItems', pre })
+          // console.log(reset)
+        })
+        .then
+        // ()=> dispatch({type : 'loadingFalse' })
+        ();
+    } catch (error) {
+      console.log(error);
+    }
+    // console.log('cache' ,cachesortBy)
+    reset ? (cachePage = null) : (cachePage = page);
+    cachesortBy = sortBy;
   }
-  console.log("cache", cachePage);
-  cachePage = page;
-  console.log("page", page);
 };
